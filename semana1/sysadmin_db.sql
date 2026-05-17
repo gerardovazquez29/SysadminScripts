3f4bc1cea41d01993f25ae060c8e913c72e5ac36
gerardovazquez29 <gerardovazquez2903@hotmail.com>
2026-05-17 09:30:19 -0600

    o

--- semana1/sysadmin_db.sql
+++ semana1/sysadmin_db.sql
@@ -1,27 +1,25 @@
-
+DROP DATABASE IF EXISTS sysadmin_db;
 CREATE DATABASE sysadmin_db;
-
-\c sysadmin_db;
 
 CREATE TABLE reportes (
     id         SERIAL PRIMARY KEY,
-    fecha      TIMESTAMP DEFAULT NOW(),
-    nombre_pc  VARCHAR(100),
+    fecha      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    nombre_pc  VARCHAR(100) NOT NULL,
     os_nombre  VARCHAR(150),
     ram_gb     NUMERIC(5,2)
 );
 
 CREATE TABLE procesos (
     id          SERIAL PRIMARY KEY,
-    reporte_id  INTEGER REFERENCES reportes(id),
-    nombre      VARCHAR(100),
+    reporte_id  INTEGER REFERENCES reportes(id) ON DELETE CASCADE,
+    nombre      VARCHAR(100) NOT NULL,
     cpu         NUMERIC(10,4)
 );
 
 CREATE TABLE servicios (
     id          SERIAL PRIMARY KEY,
-    reporte_id  INTEGER REFERENCES reportes(id),
-    nombre      VARCHAR(100),
+    reporte_id  INTEGER REFERENCES reportes(id) ON DELETE CASCADE,
+    nombre      VARCHAR(100) NOT NULL,
     estado      VARCHAR(20)
 );
 
